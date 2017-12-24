316bb3836d0c312a3e40d5b989f424ccf2a7787b
Edmond Lee <e.lee@live.ca>
2017-12-23 23:43:32 -0800

    bill_calc.py

---
+++ bill_calc.py
@@ -0,0 +1,13 @@
+
+# Restaurant Bill Calculator
+
+meal_cost = 18.92
+
+tax = 0.05
+tip = 0.12
+meal_tax = meal_cost * tax
+meal_w_tax = meal_tax + meal_cost
+meal_tip = meal_w_tax * tip 
+total = meal_w_tax + meal_tip
+
+print ("%.2f" % total)
