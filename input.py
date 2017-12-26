66a45343470f3e62949c1bf97d72df8d9cc693b2
Edmond Lee <e.lee@live.ca>
2017-12-25 18:26:07 -0800

    input.py

---
+++ input.py
@@ -0,0 +1,8 @@
+#Basic User Input
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+country = input("Enter your country: ")
+
+print ("You are " + name + ", " + age + " year(s) old, from " + country + ".")
+print ("You are %s, %s year(s) old, from %s." % (name, age, country))
