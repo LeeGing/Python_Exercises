96125153e14c5c1bd9a523739345b51e91e05da3
Edmond Lee <e.lee@live.ca>
2017-12-26 14:49:27 -0800

    bill_calc.py updated

--- bill_calc.py
+++ bill_calc.py
@@ -1,7 +1,12 @@
 # Restaurant Bill Calculator
-
-meal_cost = 18.92
-
+print ("==============================")
+print ("= Restaurant Bill Calculator =")
+print ("==============================")
+print ("= Tax - 5%                   =")
+print ("= Tip - 12%                  =")
+print ("==============================")
+meal_cost = float(input("Enter an Amount: $"),)
+print ("==============================")
 tax = 0.05
 tip = 0.12
 meal_tax = meal_cost * tax
@@ -9,4 +14,4 @@ meal_w_tax = meal_tax + meal_cost
 meal_tip = meal_w_tax * tip 
 total = meal_w_tax + meal_tip
 
-print ("%.2f" % total)
+print ("Total: $" + "%.2f" % total)
