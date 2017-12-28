ba97c8b345c531a0f038332c889e3ed0a2d6f899
Edmond Lee <e.lee@live.ca>
2017-12-28 02:33:20 -0800

    pig_latin.py updated

--- pig_latin.py
+++ pig_latin.py
@@ -1,19 +1,36 @@
-#Pig Latin Translator
+#Pig Latin Converter
+import time
 
-ay = 'AY'
-
-print ("###################################################################################")
-print ("###                             Pig Latin Converter                                ")
-print ("###################################################################################")
-user_input = input('### Enter a word: ')
-print ("###################################################################################")
-
-if len(user_input) > 0 and user_input.isalpha():
-  word = user_input.upper()
-  first_letter = word[0]
-  pig_latin = word[1:len(word)] + first_letter + ay
-  print ("### Your word: " + word + ", converted into Pig Latin is: " + pig_latin + ".")
-  print ("###################################################################################")
-else:
-  print ('Error - No word, or more than one word detected. Try again.')
-  print ("###################################################################################")
+def plc():
+	ay = 'AY'
+	print ("===================================================================================")
+	print ("###                           Pig Latin Converter                               ###")
+	print ("===================================================================================")
+	user_input = input('=== Enter a word: ')
+	print ("===================================================================================")
+	if len(user_input) > 0 and user_input.isalpha():
+	  word = user_input.upper()
+	  first_letter = word[0]
+	  pig_latin = word[1:len(word)] + first_letter + ay
+	  time.sleep(0.5)
+	  print ("Converting...")
+	  time.sleep(0.5)
+	  print ("Converting...")
+	  time.sleep(0.5)
+	  print ("Converting...")
+	  time.sleep(0.5)
+	  print ("===================================================================================")
+	  print ("Your word: " + word + ", converted into Pig Latin is: " + pig_latin + ".")
+	  print ("===================================================================================")
+	  print ("###                                  END                                        ###")
+	  print ("===================================================================================")
+	  plc()
+	else:
+	  print ('Error Detected - Try Again...')
+	  print ("===================================================================================")
+	  time.sleep(1.5)
+	  print ('Restarting Pig Latin Converter...')
+	  print ("===================================================================================")
+	  time.sleep(3)
+	  plc()
+plc()
