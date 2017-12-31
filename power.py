c8a3745b0b1e2c949ec9e094afa706ccec8248bb
Edmond Lee <e.lee@live.ca>
2017-12-31 01:20:41 -0800

    power.py

---
+++ power.py
@@ -0,0 +1,6 @@
+def power(base, exponent):  
+  result = base ** exponent
+  print ("%d to the power of %d is %d." % (base, exponent, result))
+base = int(input("ENTER BASE NUMBER: "))
+exponent = int(input("ENTER EXPONENT NUMBER: "))
+power(base,exponent)  
