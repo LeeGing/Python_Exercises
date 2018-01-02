adb122fe3c60be9fc041ceb872979b44607095d4
Edmond Lee <e.lee@live.ca>
2018-01-01 23:23:37 -0800

    power.py updated

--- power.py
+++ power.py
@@ -1,6 +1,12 @@
 def power(base, exponent):  
   result = base ** exponent
-  print ("%d to the power of %d is %d." % (base, exponent, result))
-base = int(input("ENTER BASE NUMBER: "))
-exponent = int(input("ENTER EXPONENT NUMBER: "))
+  print ("RESULT: %d to the power of %d is %d." % (base, exponent, result))
+  print ("========================================")
+print ("========================================")
+print ("========= Exponent Calculator ==========")	
+print ("========================================")
+base = int(input("= ENTER BASE NUMBER: "))
+print ("========================================")
+exponent = int(input("= ENTER EXPONENT NUMBER: "))
+print ("========================================")
 power(base,exponent)  
