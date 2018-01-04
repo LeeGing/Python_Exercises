12ebe0b3aa7d60e505b54953027029565f62b4fa
Edmond Lee <e.lee@live.ca>
2018-01-04 13:41:30 -0800

    8_ball.py updated

--- 8_ball.py
+++ 8_ball.py
@@ -1,11 +1,19 @@
 #Magic 8 Ball
 
-import random
-user_input = input("ASK THE 8 BALL YOUR QUESTION: ")
-ans = random.randint(1,3)
-if ans == 1:
-	print ("8 BALL: YES")
-elif ans == 2:
-	print ("8 BALL: NO")
-else:
-	print ("8 BALL: ASK ME AGAIN LATER")
+def shake():
+	import random
+	user_input = input("ASK THE 8 BALL YOUR QUESTION: ")
+	ans = random.randint(1,5)
+	if ans == 1:
+		print ("8 BALL: PERHAPS, YES")
+		shake()
+	elif ans == 2:
+		print ("8 BALL: UNFORTUNATELY, NO")
+		shake()
+	elif ans == 3:
+		print ("8 BALL: I CAN'T SAY.")
+		shake()
+	else:
+		print ("8 BALL: ASK ME AGAIN LATER")
+		shake()
+shake()
