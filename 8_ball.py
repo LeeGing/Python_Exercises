5211a71f66b2544422d66c7eaf8840ce344e6b32
Edmond Lee <e.lee@live.ca>
2018-01-05 23:08:36 -0800

    8_ball.py updated

--- 8_ball.py
+++ 8_ball.py
@@ -2,18 +2,26 @@
 
 def shake():
 	import random
+	print ("======================================================")
+	print ("==================== MAGIC 8 BALL ====================")
+	print ("======================================================")
+
 	user_input = input("ASK THE 8 BALL YOUR QUESTION: ")
 	ans = random.randint(1,5)
 	if ans == 1:
-		print ("8 BALL: PERHAPS, YES")
+		print ("======================================================")
+		print ("8 BALL: PERHAPS, YES.")
 		shake()
 	elif ans == 2:
+		print ("======================================================")
 		print ("8 BALL: UNFORTUNATELY, NO")
 		shake()
 	elif ans == 3:
+		print ("======================================================")
 		print ("8 BALL: I CAN'T SAY.")
 		shake()
 	else:
+		print ("======================================================")
 		print ("8 BALL: ASK ME AGAIN LATER")
 		shake()
 shake()
